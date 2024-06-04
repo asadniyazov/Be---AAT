ffa9668f28e47583b436785ababfece8502a0897
asadniyazov <bd5scjwr5@code.edu.az>
2024-06-04 16:02:27 +0400

    ss

--- FE-AUTHONDIFIKASIYA-AUTORDASIYA-TOKEN/vite-project/src/Pages/Home.jsx
+++ FE-AUTHONDIFIKASIYA-AUTORDASIYA-TOKEN/vite-project/src/Pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/user")
+   
+  }, []);
   return (
     <div>
       <h1>This is Home Page</h1>
--- FE-AUTHONDIFIKASIYA-AUTORDASIYA-TOKEN/vite-project/src/Pages/Login.jsx
+++ FE-AUTHONDIFIKASIYA-AUTORDASIYA-TOKEN/vite-project/src/Pages/Login.jsx
@@ -1,32 +1,52 @@
 import React, { useState } from 'react'
+import  Cookies from 'js-cookie'
 
-function Login() {const [email, setemail] = useState('')
-const [password, setpassword] = useState('')
-function handleSubmit(e) {
+function Login() {
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+
+
+
+
+  function handleSubmit(e) {
     e.preventDefault()
-    fetch("http://localhost:3000/user/login",{
+    fetch("http://localhost:3000/user/login", {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: "POST",
-      body: JSON.stringify({email,password})
-  })
-  .then((response)=>response.json())
-  .then((data)=>{console.log(data)})
+      body: JSON.stringify({ email, password })
+    })
+
+
+      .then((response) => response.json())
+
+
+
+      .then((data) => {
+        console.log(data)
+        Cookies.set('token', data.accessToken)
+
+      })
+
+     
   }
+
+
+
   return (
     <div>
       <h1>Login  Page</h1>
-      <form action=""  onSubmit={handleSubmit}>
-        <input value={email} onChange={(e)=>setemail(e.target.value)} type="text" />
+      <form action="" onSubmit={handleSubmit}>
+        <input value={email} onChange={(e) => setemail(e.target.value)} type="text" />
         <br />
-        <input value={password} onChange={(e)=>setpassword(e.target.value)} type="text" />
+        <input value={password} onChange={(e) => setpassword(e.target.value)} type="text" />
         <br />
         <button>Send</button>
       </form>
     </div>
-)
+  )
 }
 
 export default Login
